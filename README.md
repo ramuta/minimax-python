c2fd1aede806ea89e30158ba45357898c409132e
Matej Ramuta <matej.ramuta@gmail.com>
2016-12-12 16:27:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# miniMAX Python library
+
+### About miniMAX
+
+miniMAX is an accounting program: [http://www.minimax.si/](http://www.minimax.si/)
+
+### About miniMAX Python library
+
+This is a third-party Python library. I am in no way connected to SAOP d.o.o., the maker of miniMAX.
+
+The library is not finished yet, so any pull request is highly appreciated. Library currently uses GAE `urlfetch` lib to make 
+requests, but will be transformed to use `requests` Python library in the future.
+
+### Roadmap
+
+**DONE:**
+
+- API authentication
+- GetOrganizations
+
+**TODO:**
+
+- Implement `requests` library for API requests
+- IssuedInvoice
+- ...
+
+### Useful links:
+
+- miniMAX site: [http://www.minimax.si/](http://www.minimax.si/)
+- miniMAX API docs: [https://moj.minimax.si/SI/API/](https://moj.minimax.si/SI/API/)
+- API instructions (in Slovene): [http://help.minimax.si/sl/api-navodila-za-razvijalce](http://help.minimax.si/sl/api-navodila-za-razvijalce)
---
+++ minimax.py
@@ -0,0 +1,69 @@
+from google.appengine.api import urlfetch
+import urllib
+import logging
+import json
+
+
+class MiniMaxOrganisation:
+    def __init__(self, name, org_id, resource_url):
+        self.name = name
+        self.org_id = org_id
+        self.resource_url = resource_url
+
+
+class MiniMax:
+    def __init__(self, username, password, client_id, client_secret, grant_type="password"):
+        self.username = username
+        self.password = password
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.grant_type = grant_type
+        self.auth_result = None
+        self.auth_token = None
+        self.organisations = []
+        self.root_url = "https://moj.minimax.si/demo/si"
+
+    def connect(self):
+        """Connect to MiniMAX API, get token and a list of organizations connected to the account."""
+        get_token_url = self.root_url + "/aut/oauth20/token"
+
+        params = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "grant_type": self.grant_type,
+            "username": self.username,
+            "password": self.password
+        }
+
+        logging.info("client_secret: " + self.client_secret)
+
+        form_data = urllib.urlencode(params)
+
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+        result = urlfetch.fetch(url=get_token_url, payload=form_data, method=urlfetch.POST, headers=headers)
+
+        self.auth_result = result
+        self.auth_token = json.loads(result.content)["access_token"]
+        self._get_organizations()
+
+    def _get_organizations(self):
+        """Get a list of all organisations that belong to the account."""
+        url = self.root_url + "/api/api/currentuser/orgs"
+
+        headers = {"Authorization": "Bearer {}".format(self.auth_token)}
+
+        result = urlfetch.fetch(url=url, method=urlfetch.GET, headers=headers)
+
+        rows = json.loads(result.content)["Rows"]
+
+        for row in rows:
+            org = MiniMaxOrganisation(name=row["Organisation"]["Name"],
+                                      org_id=row["Organisation"]["ID"],
+                                      resource_url=row["Organisation"]["ResourceUrl"])
+            self.organisations.append(org)
+
+        return result
+
+    def issue_invoice(self):
+        pass
